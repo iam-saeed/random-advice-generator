363b4686e3da0b8c7627aeef02566fca52985e46
Saeed Khan <saeed@skdevelopment.org>
2021-12-10 09:46:40 -0700

    project complete :)

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [state, setState] = useState({
+    advice: '...'
+  })
+  const onClick = (e) => {
+    e.preventDefault();
+    axios.get("https://api.adviceslip.com/advice").then((res) => {
+        setState({
+        ...state,
+        advice: res.data.slip.advice
+      })
+  })
+  }
+  return (
+    <div className="App">
+      <header className="header">
+        advicePlace
+      </header>
+      <div className="advice-section">
+      <h1>Random Advice Generator</h1>
+      <br />
+      <h2>"{state.advice}"</h2>
+      <button onClick={onClick}>Click For Advice</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
